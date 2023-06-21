4b645bcd6a91494a8c1f834708b9922230c02835
Dmitriy Sapronov <dimasapronov31@gmail.com>
2023-06-21 09:52:22 +0000

    refactor: Renamed ExceptionHandler to ExceptionHandlerControllerAdvice

--- src/main/java/com/example/ratelimiter/controller/ExceptionHandlerControllerAdvice.java
+++ src/main/java/com/example/ratelimiter/controller/ExceptionHandlerControllerAdvice.java
@@ -6,19 +6,20 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
+public class ExceptionHandlerControllerAdvice extends ResponseEntityExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
+    @ExceptionHandler(NotFoundException.class)
     protected ResponseEntity<Object> handleNotFoundException(RuntimeException ex, WebRequest req) {
         String responseBody = "";
         return handleExceptionInternal(ex, responseBody, new HttpHeaders(), HttpStatus.NOT_FOUND, req);
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(BadRequestException.class)
+    @ExceptionHandler(BadRequestException.class)
     protected ResponseEntity<Object> handleBadRequestException(RuntimeException ex, WebRequest req) {
         String responseBody = "";
         return handleExceptionInternal(ex, responseBody, new HttpHeaders(), HttpStatus.BAD_REQUEST, req);
