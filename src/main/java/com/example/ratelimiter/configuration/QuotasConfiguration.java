cd2eb691e2cd9ff9ed21c169127c4f116ee897d4
Dmitriy Sapronov <dimasapronov31@gmail.com>
2023-06-22 14:04:09 +0000

    feat: rpsQuotas bean now unmodifiable

--- src/main/java/com/example/ratelimiter/configuration/QuotasConfiguration.java
+++ src/main/java/com/example/ratelimiter/configuration/QuotasConfiguration.java
@@ -1,20 +1,17 @@
 package com.example.ratelimiter.configuration;
 
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 import java.util.HashMap;
 import java.util.Map;
 
+
 @Configuration
+@ConfigurationProperties
+@Getter
 public class QuotasConfiguration {
 
-    @Bean
-    @Scope("prototype")
-    @ConfigurationProperties("rps-quotas")
-    protected Map<String, Double> getRpsQuotas() {
-        return new HashMap<>();
-    }
+    private final Map<String, Double> rpsQuotas = new HashMap<>();
 }
